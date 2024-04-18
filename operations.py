0be84b70b6028a2f1539c03babc6fa06cdd34ea0
BassedWarrior <bassedwarrior724@outlook.com>
2024-04-17 20:06:42 -0600

    Finished missing code

--- operations.py
+++ operations.py
@@ -1,8 +1,10 @@
-a = int(input(“Give me the first number:”))
-b = int(input(“Give me the second number:”))
+a = int(input("Give me the first number:"))
+b = int(input("Give me the second number:"))
 
 # Display the addition
+print(a + b)
 
 # Display the subtraction
+print(a - b)
 
 # Display multiplication
